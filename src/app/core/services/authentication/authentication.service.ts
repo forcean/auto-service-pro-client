b3ecb8eab8e39441d726d937f4ce64e62eade58e
Annop Srichan <annopsrichan@gmail.com>
2025-11-17 20:56:50 +0700

    [Implement]: added main style font and color

--- src/app/core/services/authentication/authentication.service.ts
+++ src/app/core/services/authentication/authentication.service.ts
@@ -51,20 +51,20 @@ export class AuthenticationService {
       return null;
     }
   }
-  // getAccessToken(): string | null {
-  //   return localStorage.getItem(this.ACCESS_TOKEN_NAME);
-  // }
-
-  // getRefreshToken(): string | null {
-  //   return localStorage.getItem(this.REFRESH_TOKEN_NAME);
-  // }
   getAccessToken(): string | null {
-    return this.cookieService.get(this.ACCESS_TOKEN_NAME);
+    return localStorage.getItem(this.ACCESS_TOKEN_NAME);
   }
 
   getRefreshToken(): string | null {
-    return this.cookieService.get(this.REFRESH_TOKEN_NAME);
+    return localStorage.getItem(this.REFRESH_TOKEN_NAME);
   }
+  // getAccessToken(): string | null {
+  //   return this.cookieService.get(this.ACCESS_TOKEN_NAME);
+  // }
+
+  // getRefreshToken(): string | null {
+  //   return this.cookieService.get(this.REFRESH_TOKEN_NAME);
+  // }
 
   checkAuthenticationState(): boolean {
     const token = this.getAccessToken();
