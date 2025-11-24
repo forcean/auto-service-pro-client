a4ca1503660cd598aa1ed6c2ca90b436cf1ce38b
Annop Srichan <annopsrichan@gmail.com>
2025-11-24 17:42:10 +0700

    [Implement]: added permission

---
+++ src/app/shared/enum/role.enum.ts
@@ -0,0 +1,9 @@
+export enum ROLE {
+  SO = 'SO',
+  ADM = 'ADM',
+  MNG = 'MNG',
+  ACC = 'ACC',
+  SAL = 'SAL',
+  STO = 'STO',
+  MEC = 'MEC',
+}
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { ApiPrefix } from '../enum/api-prefix.enum';
 import { IResponseMenu } from '../interface/sidebar.interface';
 import { IBaseResponse } from '../interface/base-http.interface';
+import { IUserPermission } from '../interface/user.interface';
 
 
 @Injectable({
@@ -24,6 +25,20 @@ export class UserService {
     } catch (error) {
       if (error instanceof HttpErrorResponse) {
         return error.error as IBaseResponse<IResponseMenu>;
+      } else {
+        throw error;
+      }
+    }
+  }
+
+  async getUserPermission() {
+    try {
+      const uri = this.PREFIX_USER + `/users/permission`;
+      const response = await this.httpService.get<IUserPermission>(uri);
+      return response;
+    } catch (error) {
+      if (error instanceof HttpErrorResponse && error.error) {
+        return error.error as IBaseResponse<IUserPermission>;
       } else {
         throw error;
       }
